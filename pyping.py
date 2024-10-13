3cafdcda6e831a4bd89ca2ef219daf3fe6d80e7f
Daniel Burke <danieljburke@me.com>
2024-10-13 20:42:25 +1100

    Some tweaks

--- pyping.py
+++ pyping.py
@@ -2,13 +2,13 @@ import itertools
 import select
 import socket
 import struct
+import time
 
 def internetChecksum(data):
     sum = 0
     for word in itertools.batched(data, 2):
         sum += (word[0] << 8) + word[1]
 
-    print(hex(sum))
     carry = sum >> 16
     sum = (sum & 0xFFFF) + carry
     return (~sum & 0xffff).to_bytes(2, 'big')
@@ -22,17 +22,21 @@ def ping(addr):
         # type (8 for echo request), code (always 0 under echo request), checksum (0 to start), identifier, sequence number
         icmp_header = struct.pack('bbHHh', 8, 0, 0, 0, 0)
         data = b'A' * 32
-        print(icmp_header)
         checksum = internetChecksum(icmp_header + data)
         icmp_header = struct.pack('bbBBHh', 8, 0, checksum[0], checksum[1], 0, 0)
         packet = icmp_header + data
-        sock.sendto(packet, (host, 1))
 
-        selections = select.select([sock], [], [], 2)
+        start = time.time()
+
+        sock.sendto(packet, (host, 1))
         recvData, recvAddr = sock.recvfrom(1024)
-        print(recvAddr)
+
+        end = time.time()
+        roundTripTime = end - start
+        print(f"Elapsed: {roundTripTime}s")
         
     except socket.error as e:
         raise
 
-ping("www.google.com")
+for i in range(0, 10):
+    ping("www.google.com")
